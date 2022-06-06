ecc77637c0426eec637f7e99b048211649f3b0dd
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-06-06 11:19:05 +0300

    matkaklubi esileht

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# node-js-getting-started
+# Matka app
 
 A barebones Node.js app using [Express 4](http://expressjs.com/).
 
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const app = express()
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/test', (req, res) => res.render('pages/test'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
